57d517373ec89dc517d27a93f981a48731209aa9
arsenn5 <arsenazymanbetov@gmail.com>
2022-10-07 18:28:14 +0600

    first commit

---
+++ home/homewr1.py
@@ -0,0 +1,41 @@
+class Hero:
+    def __init__(self, name, nickname, hp, damage):
+        self.name = name
+        self.nickname = nickname
+        self.hp = hp
+        self.damage = damage
+
+    def Heal(self, heal):
+        print(self.hp + 120)
+
+    def Damage2(self, damage2):
+        print(self.damage * 2)
+
+    def Greetins(self, gritins):
+        print(f'my name is', self.name)
+
+    def Brand_phrase(self, br):
+        print(f'good will win')
+
+H = Hero(name='ulugbek', nickname='Ulugnir',hp=120,damage=50)
+BlackAdam = Hero('Blid', 'Jiro', 200, 100)
+DeadPool = Hero('Whaet', 'RIP', +9999, 78)
+Blaed = Hero('Piter', 'Vampir', 230, 90)
+
+print(H.name, H.damage, H.nickname, H.hp)
+
+print(Blaed.name, Blaed.damage, Blaed.nickname, Blaed.hp)
+
+print(DeadPool.name, DeadPool.damage, DeadPool.nickname, DeadPool.hp)
+
+print(BlackAdam.name, BlackAdam.damage, BlackAdam.nickname, BlackAdam.hp)
+
+
+H.Brand_phrase(Hero)
+
+BlackAdam.Damage2(Hero)
+
+DeadPool.Heal(Hero)
+
+Blaed.Greetins(Hero)
+
--- lessons/less1.py
+++ lessons/less1.py
@@ -6,10 +6,11 @@ class Human:
     def a(self):
         print('my name is ' + self.name)
 
-    def age1(self,a):
-        print(self.age+a)
+    def age1(self, a):
+        print(self.age + a)
 
-p1 = Human(name='Abai',age=34)
-print(p1.name,p1.age)
+
+p1 = Human(name='Abai', age=34)
+print(p1.name, p1.age)
 p1.a()
 p1.age1(11)
